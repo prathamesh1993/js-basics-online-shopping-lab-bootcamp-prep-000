c7622465744fae9bb392c3b72a02621e6bc50968
PRATHAMESH PRABHUDESAI <prathamesh1993@gmail.com>
2018-10-03 00:06:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ function removeFromCart(item) {
   var our_cart = getCart();
   for (var x = 0; x < our_cart.length; x++){
     if (our_cart[x].itemName === item){
-      delete our_cart[x];
+      our_cart.splice(x,1);
       return our_cart;
     }else{
       return "That item is not in your cart.";
